58173df8069641d358b4b7e9544be9cd58dba787
Jordan Bradley <jordanbradley@Jordans-MBP.attlocal.net>
2023-02-20 20:40:14 -0600

    fixed issue of refreshing and data not loading

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,43 +1,51 @@
 import React, { useEffect, useState } from "react";
 
-import { getAllUsers} from '../utils/API';
-
-
+import { getAllUsers } from "../utils/API";
 
 const Home = () => {
+  const [userData, setUserData] = useState([]);
 
-const [userData, setUserData] = useState([]);
+  // const fetchUsers = () => {
+  //   fetch("/api/users/")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data)
+  //       setUserData(data)
+  //     })
+  // }
 
-const fetchUsers = () => {
-  fetch("/api/users/")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-      setUserData(data)
-    })
-}
+  async function fetchUsers() {
+    try {
+      const response = await getAllUsers();
+      const data = await response.json();
 
-useEffect(() => {
-  fetchUsers()
-}, [])
-// console.log(getAllUsers)
-
-console.log(userData.users)
+      setUserData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+  // console.log(getAllUsers)
+
+  console.log(userData.users);
 
   return (
     <>
       <h1>Viewing All Users</h1>
       <div>
-      {userData.users.map((data) => {
-        return(
-          <div key={data.id}>
-          <h2>{data.email}</h2>
-          </div>
-
-        )
-      })}
+        {userData.users?.map((data) => {
+          return (
+            <div key={data.id}>
+              <p>ID: {data.id}</p>
+              <p>Username: {data.username}</p>
+              <p>Username: {data.email}</p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
