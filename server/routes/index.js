9f1306d332c5838716994fb41befe3378f3e0661
Jordan Bradley <jordanbradley@unknownae52a1e34d74.attlocal.net>
2026-02-04 11:23:27 -0600

    small changes to server side code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,9 @@
 const router = require("express").Router();
-const path = require("path");
-const fs = require("fs");
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
-const buildPath = path.join(__dirname, "../../client/build");
+// If you want a 404 for unknown API routes:
+router.use((req, res) => res.status(404).send("Not Found"));
 
-if (fs.existsSync(buildPath)) {
-  router.use((req, res) => {
-    res.sendFile(path.join(buildPath, "index.html"));
-  });
-} else {
-  router.use((req, res) => res.status(404).send("Not Found"));
-}
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const path = require("path");
+const fs = require("fs");
 const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
@@ -9,23 +10,27 @@ const cwd = process.cwd();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
 const activity = cwd.includes("01-Activities")
   ? cwd.split("/01-Activities/")[1]
   : cwd;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// serve static build (if present) BEFORE the routes so /static/* is handled by express.static
+const buildPath = path.join(__dirname, "../client/build");
+if (fs.existsSync(buildPath)) {
+  app.use(express.static(buildPath));
+}
+
 app.use(routes);
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+// fallback to client index.html for single-page app routing (only if build exists)
+if (fs.existsSync(buildPath)) {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(buildPath, "index.html"));
+  });
 }
-// Servers up client on route "/"
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 db.once("open", () => {
   app.listen(PORT, () => {
